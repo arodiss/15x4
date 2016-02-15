ef36b419e9758d79acf5a77ea8a59332b16d0f80
Gleb Sidora <gleb.sidora@sandsiv.com>
2016-02-15 12:41:29 +0200

    Fixed some problems with teaser overlay

---
+++ src/AppBundle/Twig/StringUtilsExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Twig;
+
+class StringUtilsExtension extends \Twig_Extension
+{
+    /** {@inheritDoc} */
+    public function getName()
+    {
+        return 'string_utils';
+    }
+
+    /** {@inheritDoc} */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('cut', [$this, 'cut']),
+        ];
+    }
+
+    /**
+     * @param string $string
+     * @param int $length
+     * @return string
+     */
+    public static function cut($string, $length = 40)
+    {
+        return (strlen($string) > $length)
+            ? mb_substr($string, 0, $length - 4) . " ..."
+            : $string
+        ;
+    }
+}
