b87be3bb84859d95205526be4705de79c8eb2c36
arodiss <zetaro@bigmir.net>
2016-04-08 14:41:16 +0300

    Added handling of users from different providers with same email

--- src/AppBundle/Security/FOSUBUserProvider.php
+++ src/AppBundle/Security/FOSUBUserProvider.php
@@ -4,6 +4,7 @@ namespace AppBundle\Security;
 
 use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider as VendorUserProvider;
 use AppBundle\Entity\User;
+use AppBundle\Entity\Repository\UserRepository;
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\Exception\AccountNotLinkedException;
 use Doctrine\ORM\EntityManager;
@@ -13,10 +14,19 @@ class FOSUBUserProvider extends VendorUserProvider
     /** @var EntityManager */
     private $em;
 
+    /** @var UserRepository */
+    private $userRepository;
+
     /** @param EntityManager $em */
     public function setEm(EntityManager$em)
     {
         $this->em = $em;
+    }
+
+    /** @param UserRepository $userRepository */
+    public function setUserRepository(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
     }
 
     /** {@inheritdoc} */
@@ -27,6 +37,14 @@ class FOSUBUserProvider extends VendorUserProvider
         } catch (AccountNotLinkedException $e) {
             //persist user on first login
             $user = User::fromOAuthResponse($response);
+            //maybe we already know this user from different provider?
+            $alreadySignedUser = $this->userRepository->findBy(['email' => $user->getEmail()]);
+            if (count($alreadySignedUser)) {
+                /** @var User $userTmp */
+                $userTmp = $alreadySignedUser[0];
+                $userTmp->setPictureUrl($user->getPictureUrl());
+                $user = $userTmp;
+            }
             $this->em->persist($user);
             $this->em->flush();
 
