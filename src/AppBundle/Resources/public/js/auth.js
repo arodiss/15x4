3172e9d9aa012c1d485c63e5675b0c980eae276b
arodiss <zetaro@bigmir.net>
2016-04-24 13:05:34 +0300

    some improvements for responsiveness

--- src/AppBundle/Resources/public/js/auth.js
+++ src/AppBundle/Resources/public/js/auth.js
@@ -2,7 +2,11 @@ $(function() {
     $.get('/user/me/', function (response) {
         if (response.hasOwnProperty('logged_in')) {
             if (response['logged_in']) {
-                $("#avatar").attr('src', response['picture_url']).removeClass('hide');
+                var $avatar = $("#avatar");
+
+                $avatar.parent().removeClass('col-md-0').addClass('col-md-1');
+                $avatar.parents('.row').find('.col-md-10').addClass('col-md-9').removeClass('.col-md-10');
+                $avatar.attr('src', response['picture_url']).removeClass('hide');
             } else {
                 $("#login-button").removeClass('hide');
             }
