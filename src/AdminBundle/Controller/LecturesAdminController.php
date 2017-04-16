ab52354cb6a8dc130ed909def051545f250681f7
arodiss <gleb.sidora@gmail.com>
2017-04-16 12:45:14 +0300

    Added new interface to manage featured lectures

--- src/AdminBundle/Controller/LecturesAdminController.php
+++ src/AdminBundle/Controller/LecturesAdminController.php
@@ -18,6 +18,42 @@ class LecturesAdminController extends AbstractAdminController
     }
 
     /**
+     * @Extra\Route("/lectures/featured/", name="AdminLecturesFeatured")
+     * @Extra\Template("admin/featured.html.twig")
+     */
+    public function lecturesFeaturedAction()
+    {
+        return [
+            'featured_lectures' => $this->get('repository.lecture')->findByIsFeatured(1),
+            'unfeatured_lectures' => $this->get('repository.lecture')->findByIsFeatured(0),
+        ];
+    }
+
+    /**
+     * @Extra\Route("/lectures/{id}/unfeature", name="AdminLectureUnfeature")
+     */
+    public function unfeatureLectureAction(Lecture $lecture)
+    {
+        $lecture->setIsFeatured(false);
+        $this->getEm()->flush();
+        $this->addFlash('success', sprintf('Лекция `%s` убрана из рекомендованных', $lecture->getTitle()));
+
+        return $this->redirectToRoute('AdminLecturesFeatured');
+    }
+
+    /**
+     * @Extra\Route("/lectures/{id}/enfeature", name="AdminLectureEnfeature")
+     */
+    public function enfeatureLectureAction(Lecture $lecture)
+    {
+        $lecture->setIsFeatured(true);
+        $this->getEm()->flush();
+        $this->addFlash('success', sprintf('Лекция `%s` добавлена в рекомендованные', $lecture->getTitle()));
+
+        return $this->redirectToRoute('AdminLecturesFeatured');
+    }
+
+    /**
      * @Extra\Route("/lectures/{id}/edit", name="AdminLectureEdit")
      */
     public function editLectureAction(Request $request, Lecture $lecture)
