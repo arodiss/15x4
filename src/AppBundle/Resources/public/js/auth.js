420fb22ba0ae24008027727aff6c5e30697f5538
arodiss <gleb.sidora@gmail.com>
2017-04-28 16:30:06 +0300

    Improved header responsiveness

--- src/AppBundle/Resources/public/js/auth.js
+++ src/AppBundle/Resources/public/js/auth.js
@@ -3,9 +3,9 @@ $(function() {
         if (response.hasOwnProperty('logged_in')) {
             if (response['logged_in']) {
                 var $avatar = $("#avatar");
+                var avatarContainer = $avatar.parent();
 
-                $avatar.parent().removeClass('col-lg-0').addClass('col-lg-1');
-                $avatar.parents('.row').find('.col-lg-8').addClass('col-lg-7').removeClass('.col-lg-8');
+                avatarContainer.removeClass('hide');
                 $avatar.attr('src', response['picture_url']).removeClass('hide');
             } else {
                 $("#login-button").removeClass('hide');
