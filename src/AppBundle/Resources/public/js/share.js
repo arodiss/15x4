3d3846007598994c5f8542dc31cc0710174e42ba
arodiss <gleb.sidora@gmail.com>
2017-09-05 12:13:56 +0300

    fixed FB share if VK not available

--- src/AppBundle/Resources/public/js/share.js
+++ src/AppBundle/Resources/public/js/share.js
@@ -2,14 +2,14 @@ $(function() {
     var vkjs, fbjs, fjs = document.getElementsByTagName('script')[0];
 
     //fb share
-    if (null == document.getElementById('facebook-jssdk')) {
+    if (null === document.getElementById('facebook-jssdk')) {
         fbjs = document.createElement('script'); fbjs.id = 'facebook-jssdk';
         fbjs.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
         fjs.parentNode.insertBefore(fbjs, fjs);
     }
 
     //vk share
-    if (null == document.getElementById('vk-jssdk')) {
+    if (null === document.getElementById('vk-jssdk')) {
         vkjs = document.createElement('script'); vkjs.id = 'vk-jssdk';
         vkjs.src = "//vk.com/js/api/share.js";
         vkjs.charset = "windows-1251";
@@ -17,9 +17,11 @@ $(function() {
     }
     setTimeout(
         function () {
-            $(".vk-share-button").each(function () {
-                $(this).html(VK.Share.button($(this).data('href'), {type: 'link'}));
-            });
+            if (typeof(VK) !== 'undefined') {
+                $(".vk-share-button").each(function () {
+                    $(this).html(VK.Share.button($(this).data('href'), {type: 'link'}));
+                });
+            }
         },
         1000
     )
