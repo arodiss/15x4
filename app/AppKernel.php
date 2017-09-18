525e1029043cc3e62fa1b87d0e602a2e898b3349
arodiss <gleb.sidora@gmail.com>
2017-09-18 11:16:23 +0300

    upd vendors

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -34,8 +34,6 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
