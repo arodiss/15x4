fce25e8f453871844f8843e87c49a3659a559892
arodiss <gleb.sidora@gmail.com>
2017-11-07 10:37:03 +0200

    updated jQuery 2.1 to 3.2

--- src/AppBundle/Resources/public/js/init.js
+++ src/AppBundle/Resources/public/js/init.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('a[href*=#]:not([href=#]):not(.tab-toggle)').click(function() {
+    $('a[href*=\\#]:not([href=\\#]):not(.tab-toggle)').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
